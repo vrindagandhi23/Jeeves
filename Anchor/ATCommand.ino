29e6a9551cc52c36463c19974836418f0d3aca47
Nelson Vo <54444005+nelsonvo1234@users.noreply.github.com>
2025-10-23 17:02:57 -0700

    Update ATCommand.ino

--- Anchor/ATCommand.ino
+++ Anchor/ATCommand.ino
@@ -1,25 +1,32 @@
-#define TXD0 17
-#define RXD0 16
-
-HardwareSerial mySerial(2);
+#define TXD2 17
+#define RXD2 16
+#define RYUW_NRST 4
+HardwareSerial RYUW(2);
 
 void setup() {
   Serial.begin(115200);
-  mySerial.begin(115200, SERIAL_8N1, RXD0, TXD0);
-  mySerial.flush();
+  RYUW.begin(115200, SERIAL_8N1, RXD2, TXD2);
+  RYUW.flush();
+  Serial.println("Resetting RYUW");
+  pinMode(RYUW_NRST, OUTPUT);
+  digitalWrite(RYUW_NRST, LOW);
+  delay(100);
+  digitalWrite(RYUW_NRST, HIGH);
   Serial.println("ESP32 UART bridge started");
 }
 
 void loop() {
 // Forward from USB Serial → UART2
   while (Serial.available()) {
+    // Serial.print("[TX→RYU] ");
     char c = Serial.read();
-    mySerial.write(c);
+    RYUW.write(c);
   }
 
   // Forward from UART2 → USB Serial
-  while (mySerial.available()) {
-    char c = mySerial.read();
+  while (RYUW.available()) {
+    // Serial.print("[RX←RYU] ");
+    char c = RYUW.read();
     Serial.write(c);
   }
 }
