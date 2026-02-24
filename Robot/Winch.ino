ac6a426a23e2e6008725e5a8359153fcaec89c33
Nelson Vo <nelsonvo2006@gmail.com>
2026-02-24 10:34:40 -0800

    Winch testing code

---
+++ Robot/Winch.ino
@@ -0,0 +1,54 @@
+#include <ESP32Servo.h>
+
+// rn single stepping, if we need smoother/more torque we can half step
+
+// 2048 step per rotation
+const float stepAngle = 360.0 / 2048;
+
+// pins need to be defined later
+const byte StepperPins[] = {1, 2, 3, 4}; 
+int step_number = 0;
+
+void OneStep(bool dir){
+    for(int i = 0; i < 4; i++){
+        if(i == step_number){
+            digitalWrite(StepperPins[i], HIGH);
+        }
+        else{
+            digitalWrite(StepperPins[i], LOW);
+        }
+    }
+    
+    if(dir){
+        step_number = (step_number + 1) % 4;
+    }
+    else{
+        step_number = (step_number + 3) % 4;
+    }
+}
+
+void turnDegrees(int angle){
+    int steps = abs(angle) / stepAngle;
+    if(angle > 0){
+        for(int i = 0; i < steps; i++){
+            OneStep(true);
+            delay(2);
+        }
+    }
+    else if(angle < 0){
+        for(int i = 0; i < steps; i++){
+            OneStep(false);
+            delay(2);
+        }
+    }
+}
+
+void setup(){
+    for(int i = 0; i < 4; i++){
+        pinMode(StepperPins[i], OUTPUT);
+    }
+}
+void loop(){
+    turnDegrees(360);
+    turnDegrees(-360);
+}
